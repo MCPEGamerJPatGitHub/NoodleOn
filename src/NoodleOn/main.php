bc229f0d3d24b7ba0c842e4c8f5b9da92cb892aa
MCPEGamerJPatGitHub <MCPEGamerJPatGitHub@users.noreply.github.com>
2016-01-26 06:41:49 +0300

    Update and rename plugin.php to main.php

---
+++ src/NoodleOn/main.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace MCPEGamerJP
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\item\Mushroom_Soup;
+use pocketmine\player;
+use pocketmine\Listener;
+
+class main extends PluginBase{
+
+public function onLoad{
+ 
+
